f3a6d7f4a40532674e5911233cb619b7f3f999b5
Stellazen <stellazen.xd@gmail.com>
2023-07-20 14:25:54 -0300

    style/feat(card vertical): criação e responsividade

--- src/App.js
+++ src/App.js
@@ -29,25 +29,52 @@ function App() {
   }, [currentPage]);
 
 
+  const [layout, setLayout] = useState('horizontal');
+
+  const layoutChange = (event) => {
+    const buttonClicked = event.target.className;
+  
+    if (buttonClicked === 'verticalButton') {
+      setLayout('vertical');
+    } else if (buttonClicked === 'horizontalButton') {
+      setLayout('horizontal');
+    }
+  }
+
+
   return(
     <div>
       <Header />
-      <p>Modo de visualização:</p>
-      {allCards.map((item) => (
-        <HorizontalCard key={item.id} item={item}
-        imagem={item.veiculo_foto[0]}
-        cidade={item.veiculo_cidade}
-        marca={item.veiculo_marca}
-        modelo={item.veiculo_modelo}
-        cambio={item.veiculo_cambio}
-        ano={item.ano_modelo}
-        valor={item.veiculo_valor}
-        km={item.veiculo_km}
-        />
-      ))}
+      <div className='fundo'>
+        <p className='paragrafo'>Visualização:</p>
+        <div className='optionsCards'> 
+          <button
+           onClick={layoutChange} className='verticalButton'>
+           </button>
+          <button
+          onClick={layoutChange} className='horizontalButton'>
+          </button>
+        </div>
+        <div className={ `bodyPrincipal ${layout === 'horizontal' ? 'horizontal' : 'vertical'}`}>
+          {allCards.map((item) => (
+            <HorizontalCard key={item.id} item={item}
+            cardClass={layout === 'horizontal' ? 'horizontal' : 'vertical'}
+            layout={layout} // Passe o valor do estado layout aqui
+            imagem={item.veiculo_foto[0]}
+            cidade={item.veiculo_cidade}
+            marca={item.veiculo_marca}
+            modelo={item.veiculo_modelo}
+            cambio={item.veiculo_cambio}
+            ano={item.ano_modelo}
+            valor={item.veiculo_valor}
+            km={item.veiculo_km}
+            />
+          ))}
+        </div>
 
-  <div className='finalPagina' id='finalPagina'> </div> 
-</div>
+        <div className='finalPagina' id='finalPagina'> </div> 
+      </div>
+    </div>
   )
 }
 
--- src/components/HorizontalCard/HorizontalCard.jsx
+++ src/components/HorizontalCard/HorizontalCard.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import style from './HorizontalCard.module.css';
+import './HorizontalCard.css';
 import Button from '../Button/Button';
 
 function horizontalCard(props) {
     return (
-        <div className={style.principalContainer}>
+        <div className={ `principalContainer ${props.layout === 'horizontal' ? 'horizontal' : 'vertical'}`}>
 
-          <div className={style.carImage} style={{ backgroundImage: `url(${props.imagem})` }}>
-            <p className={style.cidade}>{props.cidade}</p>
+          <div className={ `carImage ${props.layout === 'horizontal' ? 'horizontal' : 'vertical'}`} style={{ backgroundImage: `url(${props.imagem})` }}>
+            <p className={ `cidade ${props.layout === 'horizontal' ? 'horizontal' : 'vertical'}`}>{props.cidade}</p>
           </div>
     
-          <div className={style.description}>
-            <h1 className={style.title}>{props.marca}</h1>
+          <div className='description'>
+            <h1 className='title'>{props.marca}</h1>
     
-            <div className={style.carModel}>
-              <h2 className={style.modelTitle}>{props.modelo}</h2>
+            <div className='carModel'>
+              <h2 className='modelTitle'>{props.modelo}</h2>
             </div>
     
-            <p className={style.paragraph}>{props.cambio}</p>
-            <p className={style.paragraph}>{props.ano}</p>
-            <h3>{Number(props.valor).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h3>
-            <p className={style.paragraph}>{props.km} km</p>
+            <p className='paragrafo'>{props.cambio}</p>
+            <p className='paragrafo'>{props.ano}</p>
+            <h3 className='valor'>{Number(props.valor).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h3>
+            <p className='paragrafo'>{props.km} km</p>
 
-            <div className={style.buttons}>
+            <div className='buttons'>
                 <Button>Simular Financiamento</Button>
                 <Button>Entrar em Contato</Button>
             </div>          
