e20e6dc32887399b691ad8db898ab05b2f80ca2c
Stellazen <stellazen.xd@gmail.com>
2023-07-21 10:43:11 -0300

    feat(filters):componentes dos filtros

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import dados from './data.json';
 import HorizontalCard from './components/HorizontalCard/HorizontalCard';
 import Header from './components/Header/Header';
-
+import FiltroCambio from './components/Filters/FiltroCambio/FiltroCambio';
+import FiltroMarca from './components/Filters/FiltroMarca/FiltroMarca';
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -41,25 +42,66 @@ function App() {
     }
   }
 
+  const [marcaFiltrada, setMarcaFiltrada] = useState('');
+  const [cambioFiltrado, setCambioFiltrado] = useState('');
+
+
+  const filtrarPorMarca = (event) => {
+    setMarcaFiltrada(event.target.value);
+  };
+
+  const filtrarPorCambio = (event) => {
+    setCambioFiltrado(event.target.value);
+  };
+
+  const filtrarCards = () => {
+    if (marcaFiltrada && cambioFiltrado) {
+      return allCards.filter(
+        (item) => item.veiculo_marca === marcaFiltrada && item.veiculo_cambio === cambioFiltrado
+      );
+    } else if (marcaFiltrada) {
+      return allCards.filter((item) => item.veiculo_marca === marcaFiltrada);
+    } else if (cambioFiltrado) {
+      return allCards.filter((item) => item.veiculo_cambio === cambioFiltrado);
+    } else {
+      return allCards;
+    }
+  };
+
+  const cardsFiltrados = filtrarCards();
 
   return(
     <div>
       <Header />
       <div className='fundo'>
         <p className='paragrafo visualizar'>Visualização:</p>
-        <div className='optionsCards'> 
-          <button
-           onClick={layoutChange} className='verticalButton'>
-           </button>
-          <button
-          onClick={layoutChange} className='horizontalButton'>
-          </button>
-        </div>
+        <section className='options'>
+          <div className='optionsCards'>
+            <button
+               onClick={layoutChange} className='verticalButton'>
+             </button>
+            <button
+              onClick={layoutChange} className='horizontalButton'>
+            </button>
+          </div>
+          
+          <FiltroCambio
+          onChange={filtrarPorCambio}
+          filtro={cambioFiltrado}
+          />
+
+          <FiltroMarca
+          onChange={filtrarPorMarca}
+          filtro={marcaFiltrada}
+          />
+            
+        </section>
         <div className={ `bodyPrincipal ${layout === 'horizontal' ? 'horizontal' : 'vertical'}`}>
-          {allCards.map((item) => (
+
+          {cardsFiltrados.map((item) => (
             <HorizontalCard key={item.id} item={item}
             cardClass={layout === 'horizontal' ? 'horizontal' : 'vertical'}
-            layout={layout} // Passe o valor do estado layout aqui
+            layout={layout}
             imagem={item.veiculo_foto[0]}
             cidade={item.veiculo_cidade}
             marca={item.veiculo_marca}
---
+++ src/components/Filters/FiltroMarca/FiltroMarca.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import style from './FiltroMarca.module.css'
+
+function FiltroMarca(props){
+    return(
+<div>
+<div>
+                <label  for="filtroMarca">Marca: </label>
+                <select className={style.selecionar} name="filtro" id="filtroMarca" onChange={props.onChange} value={props.filtro}>
+                <option value=""></option>
+                <option value="Citroen">Citroen</option>
+                <option value="Peugeot">Peugeot</option>
+                <option value="Renault">Renault</option>
+                <option value="Volkswagen">Volkswagen</option>
+                <option value="Fiat">Fiat</option>
+                <option value="Mitsubishi">Mitsubishi</option>
+                <option value="GM - Chevrolet">GM - Chevrolet</option>
+                <option value="Hyundai">Hyundai</option>
+                <option value="BMW">BMW</option>
+                <option value="Ford">Ford</option>
+                <option value="Honda">Honda</option>
+                <option value="Land Rover">Land Rover</option>
+            </select>
+            </div>
+</div>
+    )
+}
+
+// <select name="filtro" id="filtroMarca" onChange={filtrarPorMarca} value={marcaFiltrada}>
+
+export default FiltroMarca;
