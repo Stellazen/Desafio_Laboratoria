ed8aa988ff0de552d27527d2ccd0bd210cd41d6a
StellaZen <stellazen.xd@gmail.com>
2023-08-22 15:11:38 -0300

    retirada logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import style from './Header.module.css';
-import Logo from '../../images/lagartixa_verde.png'
+
 
 function Header(){
     return(
-        <header className={style.header}>
-        <img src={Logo} className={style.imgLogo} alt='imagem logo sóCarrão' />
+      <header className={style.header}>
+      <p>Versão não oficial</p>  
       </header>
     )
 }
